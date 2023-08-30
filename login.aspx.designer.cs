56189e911f0b25cc9916b30d072cb9a9eb23bf49
AzulMonk <syroknight12@gmail.com>
2023-08-30 14:05:36 +0200

    textbox

--- login.aspx.designer.cs
+++ login.aspx.designer.cs
@@ -42,15 +42,6 @@ namespace EliteHangers
         protected global::System.Web.UI.WebControls.TextBox txtEmail;
 
         /// <summary>
-        /// TextBox1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-
-        /// <summary>
         /// Label4 control.
         /// </summary>
         /// <remarks>
